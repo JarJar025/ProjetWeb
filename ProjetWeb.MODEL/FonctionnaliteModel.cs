2eb7c0163de66a3db6e7e2e99714b4f3dd118f6a
Sébastien Salle <Sebastien.salle@diiage.org>
2017-07-03 17:41:14 +0200

    Suppr projet Web

---
+++ ProjetWeb.MODEL/FonctionnaliteModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetWeb.MODEL
+{
+    public class FonctionnaliteModel
+    {
+        public int ID_Fonctionnalite { get; set; }
+        public String Nom_Fonctionnalite { get; set; }
+
+        public FonctionnaliteModel()
+        {
+
+        }
+        public FonctionnaliteModel(int fo, string nmf)
+        {
+            ID_Fonctionnalite = fo;
+            Nom_Fonctionnalite = nmf;
+        }
+    }
+}
